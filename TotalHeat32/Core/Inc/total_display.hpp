a886916c9e7bceda6962b4b0154642ce5f2857f1
IvanZend <ivanzend42@gmail.com>
2022-02-09 16:59:03 +0300

    int

--- TotalHeat32/Core/Inc/total_display.hpp
+++ TotalHeat32/Core/Inc/total_display.hpp
@@ -8,14 +8,29 @@
 #ifndef INC_TOTAL_DISPLAY_HPP_
 #define INC_TOTAL_DISPLAY_HPP_
 
+#include "stm32f4xx_hal.h"
+
+typedef struct
+{
+const uint8_t *data;
+uint16_t width;
+uint16_t height;
+uint8_t dataSize;
+} tImage;
 
 
+
+#define LCD_FRAME_BUFFER(N) ((uint32_t)(0xC0000000 + (N)*0x1D4C00))
+#define LAYER_PAGE_0		0
+#define X_SIZE	 800
+#define Y_SIZE	 480
 
 #ifdef __cplusplus
 	extern "C"
 	{
 #endif
 
+	void TFT_DrawBitmap_d(uint32_t Xpos, uint32_t Ypos, const uint8_t *bitmap, uint16_t width, uint16_t height,  uint8_t layer);
 	void test_output(void);
 
 #ifdef __cplusplus
--- TotalHeat32/Core/Src/total_display.cpp
+++ TotalHeat32/Core/Src/total_display.cpp
@@ -6,9 +6,12 @@
  */
 
 #include <total_display.hpp>
+#include <iostream>
+#include <cstring>
 
+//#include "Images/img_btn_timer_pause_rlsd.h"
 
-
+extern LTDC_HandleTypeDef hltdc;
 
 
 #ifdef __cplusplus
@@ -16,9 +19,34 @@ extern "C"
 {
 #endif
 
+void TFT_DrawBitmap_d(uint32_t Xpos, uint32_t Ypos, const uint8_t *bitmap, uint16_t width, uint16_t height,  uint8_t layer){
+	 uint32_t y,x,p=Xpos,c=0; //len=strlen((char*)bitmap);
+  /* Set the address */
+  uint32_t  address = hltdc.LayerCfg[layer].FBStartAdress +  Ypos*X_SIZE*4;
+  /* Bypass the bitmap header */
+  for(y=0;y<height;y++){
+    //for(x=1;x<width+1;x++){
+		for(x=0;x<width;x++){
+		c=0;
+		c=0x00000000;
+		c |= (bitmap[((y*width)+x)*4+3]);
+		c |= (bitmap[((y*width)+x)*4+2]) << 8;
+		c |= (bitmap[((y*width)+x)*4+1]) << 16;
+		c |= (bitmap[((y*width)+x)*4+0]) << 24;
+
+      *(uint32_t*) (address + (y+x+p)*4) = c;//TTZ_COL_ORAN;
+      if((y+x)>=(X_SIZE*Y_SIZE)) return;
+    }
+	p=p+X_SIZE-1;
+  }
+  return;
+}
+
 void test_output(void)
 {
+	//HAL_LTDC_SetAddress(&hltdc,LCD_FRAME_BUFFER(LAYER_PAGE_0), 0);
 
+	//TFT_DrawBitmap_d(0, 0, img_btn_timer_pause_rlsd.data, img_btn_timer_pause_rlsd.width, img_btn_timer_pause_rlsd.height, LAYER_PAGE_0);
 }
 
 #ifdef __cplusplus
